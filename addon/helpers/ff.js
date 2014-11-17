fe8322da9ca2e7a0cca3d31a14b75facdb41f174
Jay Phelps <hello@jayphelps.com>
2014-11-17 15:21:09 -0800

    [WIP] bound-ff helper

--- addon/helpers/ff.js
+++ addon/helpers/ff.js
@@ -1,15 +1,32 @@
+import Ember from 'ember';
 import features from '../features';
 
-export function ff( value, options ) {
+var helpers = Ember.Handlebars.helpers;
+
+export function ff(featureKey, options) {
   var fnTrue = options.fn;
   var fnFalse = options.inverse;
+  var originalContext = this;
 
-  if( features.enabled(value) ) {
-    fnTrue(this);
+  if (options.contexts && options.contexts.length) {
+    originalContext = options.contexts[0];
   }
-  else {
-    fnFalse(this);
-  }
+
+  delete options.contexts;
+
+  options.fn = function (context, options) {
+    return fnTrue.call(originalContext, originalContext, options);
+  };
+
+  options.inverse = function (context, options) {
+    if (features.logFeatureFlagMissEnabled) {
+      features.logFeatureFlagMiss(featureKey);
+    }
+
+    return fnFalse.call(originalContext, originalContext, options);
+  };
+ 
+  return helpers.boundIf.call(window.Features, featureKey, options);
 }
 
 export default ff;
--- tests/unit/helpers/ff-test.js
+++ tests/unit/helpers/ff-test.js
@@ -2,6 +2,7 @@ import {
   ff
   } from 'ember-feature-flags/helpers/ff';
 import { withFeature } from 'ember-feature-flags/tests/helpers/with-feature';
+import { withoutFeature } from 'ember-feature-flags/tests/helpers/without-feature';
 
 module('Feature Flag Helper', {
   teardown: function() {
@@ -46,3 +47,34 @@ test('it calls the true case when feature is off', function() {
   ff('some-feature', options);
 });
 
+test('it is bound to the Features object and updates when the feature is toggled on/off', function() {
+  expect(1);
+  var trueCount = 0;
+  var falseCount = 0;
+
+  function t() {
+    trueCount++;
+  }
+
+  function f() {
+    falseCount++;
+  }
+
+  var options = {
+    fn: t,
+    inverse: f
+  };
+
+  // Feature on
+  withFeature('some-feature');
+  ff('some-feature', options);
+
+  equal(trueCount, 1, 'true fn called once');
+  equal(falseCount, 0, 'false inverse fn not called');
+
+  // Feature off
+  withoutFeature('some-feature');
+
+  equal(trueCount, 1, 'true fn called once');
+  equal(falseCount, 1, 'false inverse fn called once');
+});
