077c6a2f69ad556f1bb7437f7be19947d1ec5ad6
Katie Gengler <katie@kmg.io>
2016-01-25 22:55:46 -0500

    Add RAISE_ON_DEPRECATIONS to fail builds when there are deprecations

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -37,6 +37,7 @@ module.exports = function(environment) {
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
     ENV.APP.rootElement = '#ember-testing';
+    ENV.EmberENV.RAISE_ON_DEPRECATION = true;
   }
 
   if (environment === 'production') {
