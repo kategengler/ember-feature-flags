471ce62c9feef47e683fc6d2b0563fe4940e4935
Katie Gengler <katie@kmg.io>
2025-02-11 17:47:22 -0500

    Deprecate `withFeature` test helper from old-style Ember tests

--- addon-test-support/helpers/with-feature.js
+++ addon-test-support/helpers/with-feature.js
@@ -1,6 +1,19 @@
 import { registerHelper } from '@ember/test';
 import { _enableFeature } from '../-private/enable-feature';
+import { deprecate } from '@ember/debug';
 
 registerHelper('withFeature', function withFeature(app, featureName) {
+  deprecate(
+    'The `withFeature` test helper is deprecated. Please use the `enableFeature` test helper instead with modern Ember testing syntax. See https://github.com/kategengler/ember-feature-flags#enablefeature--disablefeature',
+    false,
+    {
+      id: 'ember-feature-flags.with-feature',
+      until: '7.0.0',
+      for: 'ember-feature-flags',
+      since: {
+        enabled: '6.1.0'
+      }
+    }
+  );
   _enableFeature(app.__deprecatedInstance__, featureName);
 });
